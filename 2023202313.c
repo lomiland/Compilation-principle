ebe51c117d24cb696900749e1063b0bdf51d5dba
lomiland <rlxys_0805@qq.com>
2025-02-19 11:42:22 +0800

    2023202313function2

--- 2023202313.c
+++ 2023202313.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main() {
-    printf("This is the first lab!\\n");
+int main()
+{
+    printf("This is the first lab!\n");
+    printf("This is the second lab!\n");
     return 0;
 }
